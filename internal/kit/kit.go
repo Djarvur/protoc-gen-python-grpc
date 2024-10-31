c5dcd5c54d468e1ebef264cb44b0594c064be50e
Daniel Podolsky <1049874+onokonem@users.noreply.github.com>
2024-10-31 10:44:46 +0300

    useless code removed

--- internal/kit/kit.go
+++ internal/kit/kit.go
@@ -1,9 +1,7 @@
 package kit
 
 import (
-	"bytes"
 	"io"
-	"os"
 
 	plugin_go "github.com/golang/protobuf/protoc-gen-go/plugin"
 	"github.com/pseudomuto/protokit"
@@ -21,26 +19,5 @@ func New() Kit {
 }
 
 func (k Kit) RunPluginWithIO(p Plugin, r io.Reader, w io.Writer) error {
-	in, err := io.ReadAll(r)
-	if err != nil {
-		panic(err)
-	}
-
-	if err = os.WriteFile("testdata/in.bin", in, 0o644); err != nil {
-		panic(err)
-	}
-
-	var out bytes.Buffer
-
-	errPlugin := protokit.RunPluginWithIO(p, bytes.NewBuffer(in), &out)
-
-	if err = os.WriteFile("testdata/out.bin", out.Bytes(), 0o644); err != nil {
-		panic(err)
-	}
-
-	if _, err = io.Copy(w, &out); err != nil {
-		panic(err)
-	}
-
-	return errPlugin
+	return protokit.RunPluginWithIO(p, r, w)
 }
